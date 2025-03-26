c2b52f03cbd20c93b5d4825bfbf10e594c09c242
ahilarion <alan.hilarion@proton.me>
2025-03-26 14:14:36 +0100

    refactor(ProductRepository): change pagination limit from 14 to 13
    The pagination limit was adjusted from 14 to 13 to improve alignment with UI design requirements and enhance user experience.

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -34,11 +34,11 @@ class ProductRepository implements ProductRepositoryInterface
                     ->orWhere('brand', 'LIKE', "%$search%")
                     ->orWhere('type', 'LIKE', "%$search%")
                     ->orderBy('created_at', 'desc')
-                    ->paginate(14);
+                    ->paginate(13);
             } else {
                 $products = QueryBuilder::for(Product::class)
                     ->orderBy('created_at', 'desc')
-                    ->paginate(14);
+                    ->paginate(13);
             }
         }
 
